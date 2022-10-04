36f9a366e8d625803c4896e2db02ac42e7e04b5e
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-04 09:22:34 -0700

    ADD:betty style comment

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * This function prints a text string to the output followed by a new line
+ *
+ * and end with the value 0
+ * 
+ */
+ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
