d5c7b03f8fa7bee98f5b66f812ceb88b6f0a4dd4
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-04 09:42:49 -0700

    ADD:5-printf.c file

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * This function prints a text string followed by a new line
+ * and return the value 0 when finished
+ */
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
