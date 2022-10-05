ae1e9253891515aa0b5413ea89706efbb7182829
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-05 13:19:04 -0700

    FIX:typo in printf and include stdio lib

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - print positive or negative
@@ -14,9 +15,10 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
-		pritnf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
