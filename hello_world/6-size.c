b8bfbd753ff04fe79ee77c8d17524cc62f4ad959
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-05 07:51:43 -0700

    ADD:6-size file

---
+++ hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print a string
+ *
+ * Description: Print a string of char adding a newline with printf
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+
+	printf("Size of a char is: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int is: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int is: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int is: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float is: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
