3326a01f126d9253592de26d4306cbc4cc23f373
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-05 16:25:21 -0700

    ADD: 2-print_alphabet.c file

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet
+ *
+ * Description: Print the alphabet followed by a new line
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+
+	int c;
+
+	for (c='a'; c<='z'; c++)
+	{
+	putchar(c);
+	}
+	putchar ('\n');
+	return (0);
+}
