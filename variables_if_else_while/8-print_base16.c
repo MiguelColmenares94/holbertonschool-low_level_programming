6fe52193ecbff99259ec8d6a7b156bca72f1d6a5
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-05 19:04:54 -0700

    ADD: 8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Description: Print numbers of base 16 followed by a new line
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+
+	int c;
+
+	for (c = 48; c < 57; c++)
+	{
+	putchar(c);
+	}
+	for (c = 97; c <= 102; c++)
+	{
+	putchar(c);
+	}
+	putchar ('\n');
+	return (0);
+}
