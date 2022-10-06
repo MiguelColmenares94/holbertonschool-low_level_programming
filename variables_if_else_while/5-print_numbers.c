f997d527611ee81ac8667db2300d878a502148a7
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-05 17:19:03 -0700

    ADD: 5-print_number.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print decimal numbers
+ *
+ * Description: Print numbers from 0 to 10
+ * followed by a new line
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	int a;
+       
+	a = 0;
+
+	while (a <= 10)
+	{
+		printf("%d",a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
