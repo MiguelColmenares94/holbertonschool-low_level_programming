dc7be212aed72d34268a45dfc299917743b57f29
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-07 15:35:12 -0700

    ADD:10-add.c file

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int_add - add two integers
+ *@a
+ * Description: add two integers and return the result.
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	int c;
+	
+	c = a + b;
+
+	return (c);
+
+}
