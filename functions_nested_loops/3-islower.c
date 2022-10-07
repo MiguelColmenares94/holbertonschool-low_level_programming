1f414609def7a49f76fa26b2a55e6a04f6211307
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-06 20:40:40 -0700

    FIX:betty documentation

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 
 /**
  * _islower - check if lowercase
+ *@c: the number to be checked
  *
  * Description: Print 1 if lower case and 0 if no
  * Return: Always return 0
@@ -10,7 +11,7 @@
 int _islower(int c)
 {
 
-	if ( c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
