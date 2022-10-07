1cc156fc997037fd5dbb405bea30ad1232405029
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-07 12:44:24 -0700

    ADD: 9-times_table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - print table
+ *
+ * Description: Print 9 times table
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int file, column;
+	
+	file = 0;
+
+	while (file <= 9)
+	{
+	column = 0;
+
+	while (column <= 9)
+	{
+		_putchar((file * column));
+		_putchar(',');
+		_putchar('  ');
+		column++;
+	}
+	_putchar('\n');
+	file++;
+	}
+}
