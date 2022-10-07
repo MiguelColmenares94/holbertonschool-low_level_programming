22d3450f2635e1f469833953dfaa975abc243a6b
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-06 19:25:25 -0700

    FIX:betty style comment

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - print character
+ * main - print chars
  *
  * Description: Print lowercase alphabet followed by a newline
  * Return: Always return 0
@@ -13,11 +13,11 @@ void print_alphabet(void)
 
 	a = 97;
 
-	while(a <= 122)
+	while (a <= 122)
 	{
-	
+
 		_putchar(a);
-	
+
 		a++;
 	}
 	_putchar('\n');
