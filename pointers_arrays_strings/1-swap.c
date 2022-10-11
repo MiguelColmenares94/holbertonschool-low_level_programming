8515e2443d24037de49b6d4098926be496721163
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-11 11:50:08 -0700

    ADD: 1-swap.c file

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two parameters
+ * @p1: address1 of value to be updated
+ * @p2: address2 of value to be updated
+ *
+ * Description: swap the values of p1 and p2
+ * return: void
+ */
+
+
+void swap_int(int *p1, int *p2)
+{
+	*p1 = 42;
+	*p2 = 98;
+}
