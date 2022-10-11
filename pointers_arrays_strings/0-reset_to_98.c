6b8fee66fe9a83273ff9f10b9e2203e3544b260a
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-11 11:36:29 -0700

    ADD: 0-reset_to_98.c file

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update parameter value
+ * @p: address of value to be updated
+ *
+ * Description: Update the value of the address pointed
+ * return: void
+ */
+
+void reset_to_98(int *p)
+{
+	*p = 98;
+}
