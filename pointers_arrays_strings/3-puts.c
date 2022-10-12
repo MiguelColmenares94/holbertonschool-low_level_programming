7a458065c0df6e46a938732fe7e643cb0801decd
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-12 07:33:02 -0700

    ADD:_puts.c file

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: string to be printed
+ *
+ * Description: Get parameter str and print to stdout
+ * return: void
+ */
+
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n')
+}
