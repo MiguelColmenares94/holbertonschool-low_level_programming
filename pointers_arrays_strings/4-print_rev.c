04ee81713c4dff697f33dc68f41e8dde1003c5f0
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-12 10:12:34 -0700

    ADD:4-print_rev.c file

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _print_rev - return a string in reverse
+ * @s: string to be reversed
+ *
+ * Description: Get parameter "s" and print string in reverse
+ * return: void
+ */
+
+
+void _print_rev(char *s)
+{
+	int l, temp;
+
+	l = strlen(s);
+	temp = l - 1;
+
+	while (s[temp] >= 0)
+	{
+		_putchar(str[temp]);
+		temp--;
+	}
+	_putchar('\n');
+}
