aeded43e3ebc5189f1a403a8f5ccd2cf86185f60
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-13 06:52:21 -0700

    ADD:7-puts_half.c file

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  puts_half(char *str) - print half of a string
+ *  @str: string to be half printed
+ *
+ *  Description: Get a string and print it's second half
+ *  Return: void
+ */
+
+void puts_half(char *str);
+{
+	int a, b;
+
+	a = 0;
+	while (*(str + a) != '\0')
+	{
+		a++;
+	}
+
+	b = a / 2;
+	if (a % 2 == 1)
+	{
+		a++;
+	}
+
+	a = b;
+	while (*(str + a) != '\0')
+	{
+		_putchar(*(str + a));
+		a++;
+	}
+	_putchar('\n');
+}
