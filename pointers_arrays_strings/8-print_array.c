6979f510f2405202ba422b29d408e0ac8f23f755
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-13 08:41:00 -0700

    FIX: newline out of while

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -13,15 +13,17 @@
 
 void print_array(int *a, int n)
 {
-	int a;
+	int b;
 
-	while (a < n)
+	b = 0;
+	while (b < n)
 	{
-		printf("%d", *(a + i));
-			if (a != n - 1)
+		printf("%d", *(a + b));
+			if (b != n - 1)
 			{
 				printf(", ");
 			}
-			printf("\n");
+		b++;
 	}
+	printf("\n");
 }
