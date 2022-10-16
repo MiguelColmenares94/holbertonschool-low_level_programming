9f329efbb3e54d9dd7e88df7c74681c89c41bf5e
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-16 10:00:38 -0700

    ADD:5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * string_toupper - change lowercase to uppercase
+ * @*: string to be change
+ *
+ * Description: change lowercase to uppercase
+ * Return: string in uppercase
+ */
+
+char *string_toupper(char *string)
+{
+	int a;
+
+	a = 0;
+	while (*(string + a) != '\0')
+	{
+		if(*(string + a) >= 97 && *(string + a) <= 122)
+			*(string + a) -= 32;;
+		a++;
+	}
+	return (string);
+}
