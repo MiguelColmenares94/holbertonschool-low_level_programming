3bd9dd9d4c123e2c7a63d7b51843d53ad9651033
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-16 14:32:09 -0700

    ADD:2-strchr.c file

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr -  locates a character in a string.
+ * @s: string to be checked
+ * @c: character to be located
+ *
+ * Description:  locates a character in a string.
+ * Return: s[first occurrence of c]
+ */
+
+
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	a = 0;
+	while (*(s + a) != '\0')
+	{
+		if (*(s + a) == c)
+		{
+			return((s + a));
+		}
+		a++;
+	}
+	return ('\0');
+
+}
