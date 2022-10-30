cfc1fed3f54602f80252995672c70d0eacc65d66
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-29 22:27:40 -0700

    FIX:delete strlen

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -23,8 +23,8 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	}
 
-	st1 = strlen(s1);
-	st2 = strlen(s2);
+	for (st1 = 0; *(s1 + st1) != '\0'; st1++);
+	for (st2 = 0; *(s2 + st2) != '\0'; st2++);
 
 	a = malloc(st1 + st2 + 1);
 	if (a == 0)
