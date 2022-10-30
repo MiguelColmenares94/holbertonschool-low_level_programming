a155a8d2bb0e4b0fab7c897052d24c65fcb9d02f
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-29 22:54:37 -0700

    ADD:4-free_grid.c file

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * free_grid -  frees a 2 dimensional grid
+ * @grid: 2d array
+ * @height: height of matrix
+ *
+ * Return: void
+ */
+
+int free_grid(int **grid, int height)
+{
+	free(grid);
+}
