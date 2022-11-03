64a5e3e134a987471532be486d12f214c26e7bdb
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-11-03 09:32:05 -0700

    ADD: malloc

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -14,8 +14,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
-	my_dog->name = name;
-	my_dog.age = age;
-	my_dog->owner = owner;
+	if (!d)
+	{
+		d = malloc(sizeof(struct dog));
+	}
+	d->name = name;
+	d-<age = age;
+	d->owner = owner;
 }
