70dc0e2e5ee4bd2f0ff0415cb1420424f1fffb9f
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-11-05 08:28:05 -0700

    FIX:redefine function

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,46 +14,37 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *newName, newOwner;
-	int newNameCopy, newOwnerCopy;
+	char *newName, *newOwner;
 	dog_t *new_dog;
 	
 	new_dog = malloc(sizeof(dog_t));
-	if (!*new_dog)
+	newName = malloc(sizeof(name));
+	newOwner = malloc(sizeof(owner));
+
+	char* strcpy(char* newName,const char* name);
+	char* strcpy(char* newOwner,const char* owner);
+	
+	if (!new_dog)
 	{
 		return (NULL);
 	}
 	if (name != NULL)
 	{
-		newName = malloc(sizeof(name));
 		if (!newName)
 		{
 			free(new_dog);
 			return (NULL);
 		}
-		for (newNameCopy = 0; newNameCopy != '\0'; newNameCopy++)
-		{
-			*(newName + newNameCopy) = *(name + newNameCopy);
-
-		}
-		*(newName + newNameCopy) = *(name + newNameCopy);
 		new_dog->name = newName;
 	}
 	if (owner != NULL)
 	{
-		newOwner = malloc(sizeof(owner));
 		if (!newOwner)
 		{
 			free(newName);
 			free(new_dog);
 			return (NULL);
 		}
-		for (newOwnerCopy = 0; newOwnerCopy != '\0'; newOwnerCopy++)
-		{
-			*(newOwner + newOwnerCopy) = *(owner + newOwnerCopy);
-
-		}
-		*(newOwner + newOwnerCopy) = *(owner + newOwnerCopy);
 		new_dog->owner = newOwner;
 	}
 	if (age >= 0)
