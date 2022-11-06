b46c60e4420e49366b1c34770ce8daa553eb34a7
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-11-06 12:52:24 -0800

    ADD:2-print_strings.c file

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - print a string
+ *
+ * @separator: string to print
+ * @n: number of strings
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list str;
+	char *t;
+	unsigned int a;
+
+	va_start(str, n);
+	for (a = 0; a < n; a++)
+	{
+		t = va_arg(str, char *);
+		if (t == NULL)
+		{
+			t = "(nil)";
+		}
+		printf("%s", t);
+		if (separator != NULL && a != n - 1)
+			printf("%s", separator);
+	}
+	printf('\n');
+	va_end(str);
+}
