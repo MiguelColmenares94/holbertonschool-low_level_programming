fed2e175e247b8d60dd6eb0a8374dceb736a845a
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-11-06 08:40:29 -0800

    ADD:first commit

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ *
+ * @name: name to print
+ * @f: function that will print name
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+	{
+		name = malloc(sizeof(char *));
+		if (name == NULL)
+		{
+			return;
+		}
+	}
+	if (f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
+
+
