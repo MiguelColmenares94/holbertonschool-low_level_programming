a9ac1a8a2edf94fd4221b3a74dcec501de14f378
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-11-16 19:11:03 -0800

    Innitial commit

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - Print elements of list_t
+ * @h: pointer to structure list_t
+ *
+ * Return: total nodes of list
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+	h = h->next;
+	nodes++;
+	}
+	return (number_nodes);
+}
