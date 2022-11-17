c8f51407bed4cf08234a1ad7c4779f926eda2a56
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-11-16 20:56:08 -0800

    FIX:BETTY

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -14,15 +14,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (!*head && idx > 0)
 		return (NULL);
+
 	n_node = malloc(sizeof(listint_t));
 	if (!n_node)
 		return (NULL);
 	n_node->n = n;
 	n_node->next = NULL;
-	if (!idx)
+
+	if (idx == 0)
 		n_node->next = *head;
 		*head = n_node;
 		return (n_node);
+
 	aux_head = *head;
 	while (aux_head)
 	{
@@ -30,9 +33,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			n_node->next = aux_head;
 			p_node->next = n_node;
 			return (n_node);
-		p_node = aux_head;
-		aux_head = aux_head->next;
-		ctr++;
+
+	p_node = aux_head;
+	aux_head = aux_head->next;
+	ctr++;
 	}
 	if (ctr == idx)
 		p_node->next = n_node;
