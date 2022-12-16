dc792fcb4596404c5f0ec4fdb7a874d370f3105c
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-12-15 22:51:12 -0800

    ADD:Solution to exercise 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,13 @@
+i#include "lists.h"
+
+/**
+ * free_dlistint - Free a doubly linked list
+ * @head: Address of linked list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp = NULL;
+
+	for (; head; head = head->next, free(tmp))
+		tmp = head;
+}
