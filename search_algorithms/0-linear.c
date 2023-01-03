47089d09c12f18f414254c1002f63ad1c0b9265b
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-02 19:43:56 -0800

    Initial commit

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array
+ * of integers using the Linear search algorithm
+ * @array: array to search the key
+ * @size: size of the array with the key
+ * @value: key to search for into the array
+ *
+ * Return: 0 if found key, -1 if key is not found.
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	i = 0;
+	while (i < size)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+
+		i++;
+	}
+
+	if (!array || array[i] == "\0")
+	{
+		return (-1);
+	}
+}
+
+
